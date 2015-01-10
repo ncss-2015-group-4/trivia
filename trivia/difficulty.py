e53bbca9e04a9be1322d2d218ec869cdde3b3aeb
musomaddie <musomaddy@gmail.com>
2015-01-10 11:54:03 +1100

    added to the outline for the difficulty function.

--- trivia/difficulty.py
+++ trivia/difficulty.py
@@ -9,7 +9,29 @@ EXPERT = 3
 def difficulty(people_answers):
 	'''This is finding the difficulty level of a question.
 	people_answers is a list of the skill level of the person and their answer to the question.'''
-	return 0
+	sum = 0
+	for person, correct in people_answers:
+		if correct:
+			if person == BEGINNER:
+				sum += EASY
+			elif person == INTERMEDIATE:
+				sum += MEDIUM
+			elif person == EXPERT:
+				sum += HARD
+		else:
+			if person == BEGINNER:
+				sum += MEDIUM
+			elif person == INTERMEDIATE:
+				sum += HARD
+			elif person == EXPERT:
+				sum += HARD
+	average = sum/len(people_answers)
+	if average == 1:
+		print("Easy")
+	elif average == 2:
+		print("Average")
+	elif average == 3:
+		print("Hard")
 	
 if __name__ == '__main__':
 	print(difficulty([(INTERMEDIATE, True)]))
