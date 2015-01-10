dc03c223e80a75902a2af65dc6743205654dabc1
Harrison <harrison(at)theshoebridges(dot)com>
2015-01-10 12:02:44 +1100

    Added User model
    Has class methods:
    
    * find_by_id
    * find_by_username
    * create
    * delete_by_id
    * delete_by_username
    
    and the methods
    
    * authenticate

--- db/models.py
+++ db/models.py
@@ -1,1 +1,51 @@
 # The models for Trivia
+
+class User:
+    def __init__(self, _id, username, password_hash, email):
+        self.id = _id
+        self.username = username
+        self.password = password_hash
+
+    def authenticate(username, password):
+        raise NotImplementedError()
+        return False
+    
+    @classmethod
+    def find_by_id(cls, id):
+        raise NotImplementedError()
+        return cls()
+    @classmethod
+    def find_by_username(cls, username):
+        raise NotImplementedError()
+        return cls(0, username, "pw", "email")
+
+    @classmethod
+    def create(cls, username, password, email):
+        #get id from sql?
+        _id = 0
+        raise NotImplementedError()
+        return cls(_id, username, hash_password(password), email)
+
+    @classmethod
+    def delete_by_id(cls, id):
+        raise NotImplementedError()
+        #DELETE ROW WHERE id = ?
+
+        return True
+
+    @classmethod
+    def delete_by_username(cls, username):
+        raise NotImplementedError()
+        #DELETE ROW WHERE username = ?
+        return True
+
+
+
+def hash_password(string):
+    raise NotImplementedError()
+    return string
+
+    
+
+    
+        
