3922c5f04538b06c4a8310d28f33c4d22b238615
musomaddie <musomaddy@gmail.com>
2015-01-10 11:43:54 +1100

    added the outline for the difficulty function.

--- trivia/difficulty.py
+++ trivia/difficulty.py
@@ -1,1 +1,16 @@
-#this is where the difficulty function will go 
+EASY = 1
+MEDIUM = 2
+HARD = 3
+
+BEGINNER = 1
+INTERMEDIATE = 2
+EXPERT = 3
+
+def difficulty(people_answers):
+	'''This is finding the difficulty level of a question.
+	people_answers is a list of the skill level of the person and their answer to the question.'''
+	return 0
+	
+if __name__ == '__main__':
+	print(difficulty([(INTERMEDIATE, True)]))
+	
