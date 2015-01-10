44d554660f2199ef76d3815f300f0b3da54c3c08
ZxZombieSlayaxZ <emeliofox@gmail.com>
2015-01-10 16:13:43 +1100

    another update
    doesnt go anywhere if empty fields and stuff

--- login_handler.py
+++ login_handler.py
@@ -1,14 +1,10 @@
 def login_handler(request):
-    username = request.get_field("username")
-    password = request.get_field("password")
-    print(username)
-    print(password)
     request.write("""<!DOCTYPE html>
 <html>
 <body>
 <h1>
 oginlay inway erehay!!!
-<form method=post>
+<form method="post">
 Email/Username:<br>
 <input type="text" name="username">
 <br>
@@ -20,3 +16,14 @@ Password:<br>
 </body>
 </html>
 """)
+
+
+def login_handler_post(request):
+    username = request.get_field("username")
+    password = request.get_field("password")
+    if username == None or username == '' or password == None or password == '':
+       request.redirect("/login")
+       return
+    print(username)
+    print(password)
+    request.redirect("/")
