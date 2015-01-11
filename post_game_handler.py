6c82d865f9b87ebc83b8972455cf42ec9207e9f0
Ben <benji-cool@live.com.au>
2015-01-11 16:50:14 +1100

    Templating Implementation
    templating is now on all the page requests except login

--- post_game_handler.py
+++ post_game_handler.py
@@ -1,3 +1,5 @@
+from templating import render_template
+
 def post_game_handler(request):
-    post_game_page = open('static/postgamelobby.html')
-    request.write(post_game_page.read())
+    post_game_page = render_template('static/postgamelobby.html', {})
+    request.write(post_game_page)
