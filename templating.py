d51b5d75e33514718bf8ed5f83e0694c9a42c12d
lkw657 <lkw657@users.noreply.github.com>
2015-01-11 17:48:53 +1100

    Started documenting and fixing the formatting

--- templating.py
+++ templating.py
@@ -5,37 +5,59 @@ This code parses the templates into python and html.
 The python code can then be evaluated and added to the html to be displayed by a web browser
 '''
 
-IF_TAG=' if '
+IF_TAG = ' if '
 INCLUDE_TAG = ' include '
 FOR_TAG = ' for '
 
 class ParseError(Exception):
+    '''
+    Defines an exception for errors encounterd while parsing
+    '''
     pass
 
 class Node(object):
+    '''
+    Defines a basic node object
+    '''
     def __init__(self, content):
         self.content = content
-    def eval(self):
+    def eval(self, scope):
         raise NotImplementedError()
 
 class PythonNode(Node):
+    '''
+    Defines a node for the execution of python code
+    Syntax: {{<python_expression}}
+    '''
     def eval(self, scope):
+        #return the execution of the python code
         return eval(self.content, scope)
 
 class TextNode(Node):
+    '''
+    Defines a node for html code/other stuff that is not part of the templating language
+    '''
     def eval(self, scope):
         return self.content
 
 class GroupNode(Node):
+    '''
+    Defines a node to group nodes in the same area, e.g within a for/if statement
+    '''
     def __init__(self, children):
         self.children = children
     def eval(self, scope):
         result = ''
+        #evaluate all the child nodes and add them to output
         for node in self.children:
             result += str(node.eval(scope))
         return result
         
 class IfNode(Node):
+    '''
+    Defines a node for execution if statements
+    Syntax: {% if <condition> %}<stuff>{% end if %}
+    '''
     def __init__(self, predicate, true_node):
         self.predicate = predicate
         self.true_node = true_node
@@ -47,6 +69,10 @@ class IfNode(Node):
             return ''
 
 class IncludeNode(Node):
+    '''
+    Defines a node for including other html files
+    Syntax: {% include <file> %}
+    '''
     def __init__(self, path):
         self.path = path
         
@@ -57,10 +83,15 @@ class IncludeNode(Node):
         return Parser(template).eval(scope)
             
 class ForNode():
+    '''
+    Defines a node for executing for loops
+    Syntax: {% for <var_name> in <itterable_name> %}<stuff>{% end for %}
+    '''
     def __init__(self, var_name, itterable_name, true_node):
         self.var_name = var_name
         self.itterable_name = itterable_name
         self.true_node = true_node
+    
     def eval(self, scope):
         output = ''
         modified_scope = scope
@@ -72,6 +103,8 @@ class ForNode():
 
 class Parser(object):
     '''
+    Class used to parse a template file into html
+    
     >>> Parser("abcd{{1+1}}efgh{% if 1==1 %}2{% end if %}{% if 1==3 %}3{% end if %}1234").eval({})
     'abcd2efgh21234'
     >>> Parser("{% if 1==1 %}2{% end if %}{{5*2}}{% if 1==3 %}3{% end if %}1234").eval({})
@@ -96,9 +129,12 @@ class Parser(object):
         self.length = len(tokens)
 
     def _parse_python_node(self):
-        #need to account for python node in python node
+        '''
+        Function to parse a python node
+        '''
         content = ''
         content += self.tokens[self.index]
+        #all everything before the end of the node into content
         while self.read_next() != '}':
             self.next()
             content += self.tokens[self.index]
@@ -106,12 +142,15 @@ class Parser(object):
                 raise ParseError("Unexpected end of input.")
         self.next()
         if self.read_next() != '}':
-            raise ParseError("'}' expected")
+            raise ParseError("'}' expected at end of python node")
         self.next()
         self.next()
         return PythonNode(content)
 
     def _parse_text_node(self):
+        '''
+        Function to parse a text node
+        '''
         content = ''
         content += self.tokens[self.index]
         while not self.is_end() and self.read_next() != '{':
@@ -121,46 +160,58 @@ class Parser(object):
         return TextNode(content)
         
     def _parse_if_node(self):
+        '''
+        Function to parse an if node
+        '''
         predicate = ''
         predicate += self.tokens[self.index]
+        #find the predicate for the if statement
         while self.read_next() != '%':
             self.next()
             predicate += self.tokens[self.index]
             if self.is_end():
                 raise ParseError("Unexpected end of input.")
         self.next()
+        #check for the end of the if decleration
         if self.read_next() != '}':
             raise ParseError("'}' expected")
         self.next() 
         self.next()
+        #create a GroupNode for all the stuff in the for loop
         true_node = self._parse_group_node('{% end if %}')
         return IfNode(predicate, true_node)
         
     def _parse_for_node(self):
+        '''
+        Function to parse a for node
+        '''
         var_name = ''
         itterable_name = ''
         var_name += self.tokens[self.index]
+        #find the name of the variable to store the value in the itterable
         while self.read_next() != ' ':
             self.next()
             var_name += self.tokens[self.index]
             if self.is_end():
-                raise ParseError("Unexpected end of input.")   
+                raise ParseError("Unexpected end of input.")
+        #check if the variable name exists
         if var_name=='':
             raise ParseError('Missing variable name.')
         self.next()
-        
+        #check for " in "
         if self.tokens[self.index:self.index+4]!= ' in ':
             raise ParseError("'in' expected.")
         self.index+=3
-        
+        #find the name of the itterable that is being looped through
         while self.read_next() != ' ':
             self.next()
             itterable_name += self.tokens[self.index]
             if self.is_end():
                 raise ParseError("Unexpected end of input.")
+        #check if the itterable name exists
         if itterable_name == '':
             raise ParseError("Missing name for itterable.")
-            
+        #check for the end of the for decleration
         self.next()
         if self.read_next() != '%':
             raise ParseError("'%' expected")
@@ -169,10 +220,14 @@ class Parser(object):
             raise ParseError("'}' expected")
         self.next() 
         self.next()
+        #create a GroupNode for all the stuff in the foor loop
         true_node = self._parse_group_node('{% end for %}')
         return ForNode(var_name, itterable_name, true_node)
         
     def _parse_include_node(self):
+        '''
+        Function to parse an include node
+        '''
         path = ''
         path += self.tokens[self.index]
         while self.read_next() != '%':
@@ -188,6 +243,9 @@ class Parser(object):
         return IncludeNode(path)
     
     def _parse_group_node(self, end):
+        '''
+        Function to group nodes in similar areas together into group nodes
+        '''
         children = []
         while self.tokens[self.index:self.index+len(end)] != end or (end == '' and not self.is_end()):
             if self.is_end():
@@ -219,6 +277,9 @@ class Parser(object):
         return GroupNode(children)
         
     def parse(self):
+        '''
+        Function to start parsing
+        '''
         node = self._parse_group_node('')
         if not self.is_end():
             raise ParseError("Extra input")
@@ -245,4 +306,3 @@ def render_template(path, scope):
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-    
