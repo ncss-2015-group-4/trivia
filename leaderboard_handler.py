e0c056f7e190c5104707e8d0203f7ba27020870d
KirisakiJack <darklord2738@hotmail.co.uk>
2015-01-12 11:59:31 +1100

    Put in names for the leaderboard
    Used Python and now database people can change the variables for names

--- leaderboard_handler.py
+++ leaderboard_handler.py
@@ -1,5 +1,7 @@
 from templating import render_template
 
 def leaderboard_handler(request):
-    leaderboard = render_template('static/leaderboard.html', {})
+    names = ["John", "Jack", "Kenni", "Ben", "Tony"]
+    
+    leaderboard = render_template('static/leaderboard.html', {"people": names})
     request.write(leaderboard)
