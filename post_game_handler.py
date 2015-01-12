edd16ee718331c9229aeac9998753d767027476b
Alexander <alexcollins010@gmail.com>
2015-01-13 06:33:50 +1100

    fix score again maybe this is how I'm testing bad

--- post_game_handler.py
+++ post_game_handler.py
@@ -5,9 +5,10 @@ from db.models import User
 def post_game_handler(request, score):
     u_id = request.get_secure_cookie ('user_id')
     u_name = ""
+    score = str(int(score) + 1)
     if u_id is not None:
         u_id = u_id.decode("UTF-8")
         u_name = User.find(user_id=u_id)
         u_name = u_name.username.lower().capitalize()
-    post_game_page = render_template('static/postgamelobby.html', {"user_name": u_name, "score":str(int(score+1))})
+    post_game_page = render_template('static/postgamelobby.html', {"user_name": u_name, "score":score})
     request.write(post_game_page)
