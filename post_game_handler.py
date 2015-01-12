50f669fb852ff112ea570a405a551f4e33a666d5
Alexander <alexcollins010@gmail.com>
2015-01-13 06:32:37 +1100

    fix score index

--- post_game_handler.py
+++ post_game_handler.py
@@ -9,5 +9,5 @@ def post_game_handler(request, score):
         u_id = u_id.decode("UTF-8")
         u_name = User.find(user_id=u_id)
         u_name = u_name.username.lower().capitalize()
-    post_game_page = render_template('static/postgamelobby.html', {"user_name": u_name, "score":score})
+    post_game_page = render_template('static/postgamelobby.html', {"user_name": u_name, "score":str(int(score+1))})
     request.write(post_game_page)
