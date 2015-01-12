e7963dbe948a5dc299fd70e35c92c31425eef93d
lkw657 <lkw657@noreply.github.com>
2015-01-12 14:32:37 +1100

    Moved docstring testing into a function

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import subprocess
 
-print("Testing templating docstrings")
-x=subprocess.Popen(["c:/python34/python.exe","templating.py"], stderr=subprocess.PIPE)
-if x.stderr.read()==b'':
-    print("OK")
-else:
-    print(x.stderr.read())
-print("Finished testing templating docstrings")
-print("\n")
+def Test_docstrings(file):
+    print("Testing docstrings in "+file)
+    process=subprocess.Popen(["c:/python34/python.exe",file], stderr=subprocess.PIPE)
+    if process.stderr.read()==b'':
+        print("OK")
+    else:
+        print(process.stderr.read())
+    print("\n")
 
+Test_docstrings("templating.py")
 subprocess.call(["c:/python34/python.exe","-m","tornado.testing","tests/pages.py"])
 
 input("----Press enter to continue----")
