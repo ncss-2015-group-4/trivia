182e6cd0081f57e658d2c5ef638e6579755ea75d
lkw657 <lkw657@noreply.github.com>
2015-01-12 15:31:22 +1100

    Added some more tests
    Added some tests for login and registration for when the database is set
    up

--- tests/pages.py
+++ tests/pages.py
@@ -11,42 +11,49 @@ class HTTPTestCase(AsyncHTTPTestCase):
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
     
-    def test_01_view_login(self):
+    def test_01_login_and_register(self):
         url = '/login'
         headers = {'method': 'GET'}
-        self.check_page(url, **headers)
+        self.check_page(url, **headers)#test if the login page can be accessed
         
-    def test_02_view_user(self):
         url = '/user'
         headers = {'method': 'GET'}
-        self.check_page(url, **headers)
+        self.check_page(url, **headers)#test if the user page can be accessed
         
-    def test_03_view_profile(self):
+        #url = '/user'
+        #headers = {'method': 'POST', 'body': b'username=someUser&password=pass&email=someUser@someDomain.com'}
+        #self.check_page(url, **headers)#test if the registration form works
+        
+        #url = '/login'
+        #headers = {'method': 'POST', 'body': b'username=someUser&password=pass'}
+        #self.check_page(url, **headers)#test if the login form works
+        
+    def test_02_view_profile(self):
         url = '/profile'
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
         
-    def test_04_view_submit(self):
+    def test_03_view_submit(self):
         url = '/submit'
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
         
-    def test_05_view_question(self):
+    def test_04_view_question(self):
         url = '/question'
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
         
-    def test_06_view_pre_game(self):
+    def test_05_view_pre_game(self):
         url = '/pre_game'
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
         
-    def test_07_view_game(self):
+    def test_06_view_game(self):
         url = '/game'
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
         
-    def test_08_view_post_game(self):
+    def test_07_view_post_game(self):
         url = '/post_game'
         headers = {'method': 'GET'}
         self.check_page(url, **headers)
