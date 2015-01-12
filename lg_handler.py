0d9f692c3f0cdca4f5a1fdb573cb0c7f83a0739b
ZxZombieSlayaxZ <emeliofox@gmail.com>
2015-01-12 21:08:31 +1100

    Merge branch 'tornado-swag' of https://github.com/ncss-2015-group-4/trivia into tornado-swag
    Conflicts:
    	static/header.html

--- lg_handler.py
+++ lg_handler.py
@@ -1,4 +1,7 @@
+from templating import render_template
+
 def logout(response):
 	response.clear_cookie('user_id')
-	response.redirect("/l")
+	page = render_template('static/logout.html', {})
+	response.write(page)
 	return
