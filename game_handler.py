91c2c547ea358d1fff3d426838aba3a9dae6d192
unknown <hsho9870@WJSWHG2S.internal.usyd.edu.au>
2015-01-12 20:15:17 +1100

    game_handler: prettify ALL the code

--- game_handler.py
+++ game_handler.py
@@ -5,18 +5,21 @@ def game_handler(request):
     category_id = request.get_field("category_id")
     difficulty = request.get_field("difficulty")
     user_id_cookie = request.get_secure_cookie("user_id")
-    if (user_id_cookie is not None):
+
+    if user_id_cookie is not:
     	user = User.find(id=int(user_id_cookie.decode()))
 
-    	if (category_id is not None and difficulty is not None):
+    	if category_id and difficulty:
     		game = Game.create(user.id, category_id)
 
 def get_question(request, game_id, question_index):
 	game = Game.find(id=int(game_id))
-	if (game is not None):
+	if game:
 		question = game.get_question(int(question_index))
+		answers = game.get_answers(int(question_index))
 
-		request.write(render_template('static/question.html', {question: question}))
+		request.write(render_template('static/question.html',
+					{question: question, answers: answers, question_index: question_index}))
 		return
 	request.redirect("/404kid")
 
@@ -24,16 +27,17 @@ def submit_question_handler(request):
 	game_id = request.get_field("game_id")
 	user_id_cookie = request.get_secure_cookie("user_id")
 
-	question_text = request.get_field("question_text")
+	question_id = request.get_field("question_id")
+	answer_id = request.get_field("answer_id")
 
-	if (game_id is not None and user_id_cookie is not None):
+	if game_id and user_id_cookie and question_id and answer_id:
 		game = Game.find(id=int(game_id))
 		user = User.find(id=int(user_id_cookie.decode()))
 
-		if (game is not None and user is not None):
-			if (game.user_id == user.id):
-				if (game.submit_answer(question_text)):
-					request.redirect('/play/{}'.format(game.question_index))
+		if game and user:
+			if game.user_id == user.id:
+				if game.submit_answer(question_id)):
+					request.redirect('/play/{0}'.format(game.question_index))
 					return
 
-
+	request.redirect("/404")
