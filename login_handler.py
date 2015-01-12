302f850f6466611f4da9cb1da661e5cf21a4275c
ZxZombieSlayaxZ <emeliofox@gmail.com>
2015-01-13 01:24:58 +1100

    Merge and some changes to login stuff

--- login_handler.py
+++ login_handler.py
@@ -1,11 +1,9 @@
 from db.models import User
 from templating import render_template
 
-error = ""
-
-def login_handler(request):
-    print(error)
-    login_page = render_template('static/login.html', {"error_message": error})
+def login_handler(request, **kwargs):
+    scope = {"error_message": kwargs.get("error","")}
+    login_page = render_template('static/login.html', scope)
     request.write(login_page)
     
 
@@ -21,7 +19,7 @@ def login_start(response, user_id):
     response.clear_cookie('user_id')
     if not response.get_secure_cookie("user_id"): #checks for cookie
         response.set_secure_cookie("user_id", str(user_id)) #creates a new cookie
-    response.redirect("/")
+    response.redirect("/profile")
     
 
 #======================================
@@ -60,8 +58,7 @@ def login_handler_post(request):
                error = "Password/Username is incorrect"
         else:
             error = "User doesn't exist"
-    login_handler(request) #cannot find username or email in database!
-    print(error)
+    login_handler(request, error=error) #cannot find username or email in database!
     return
 #======================================
 # Does all the signup handling:
@@ -81,19 +78,17 @@ def signup_handler_post(request):
     username = request.get_field("username")
     password = request.get_field("password")
     email = request.get_field("email")
+   
     if username == None or username == '' or password == None or password == '':
         request.redirect("/") #Says that the user is missing a feild
         return
-    
     user_data = User.find(username=username) #requests a database entry with the user's username
     if user_data == None:    #checks that there is a row in the database that 
         if User.find(email=email) != None:
-            request.redirect("/") #Says that the user email address is already in use
-            return 
+            error = "Email already in use"
         else:
             User.create(username, password, email) #Creates a new entry into the db
-            request.redirect("/") #sends user a thanks pafe or profile page?
-            return 
+            login_start(request, user_data.id)
     else:
-        request.redirect("/") #Says that the user already has a row in database
-        return 
+        error = "Username already in use"
+    login_handler(request, error=error) 
