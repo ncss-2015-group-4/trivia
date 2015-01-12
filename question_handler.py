f3a2d8b75720279692ee7ce95a5fff1867865a7f
ArethaPeethamparam <s128385@avilacollege.vic.edu.au>
2015-01-13 01:39:08 +1100

    Added categories from the database to the question_handler

--- question_handler.py
+++ question_handler.py
@@ -1,5 +1,6 @@
 from db.models import TriviaQuestion
 from db.models import Answer
+from db.models import Category
 from templating import render_template
 
 def new_question_handler(request):
@@ -17,45 +18,13 @@ def new_question_handler(request):
   Answer.create(question.id, False, wrong_answer_2)
   Answer.create(question.id, False, wrong_answer_3)
   request.redirect('/category/' + category)
-  
-def new_question_form(request):
-	question_new = render_template('static/submit.html', {})
-	request.write(question_new)
-'''
-   request.write("""<!DOCTYPE html>
-<html>
-<body>
-<h1>
-New Question Form :)
-</h1>
-<form method="post">
-<select name="categories">
-  <option value="1">Harry Potter</option>
-  <option value="2">Doctor Who</option>
  
-</select>
-Question:<br>
-<input type="text" name="question">
-<br>
-Correct answer:<br>
-<input type="text" name="correct_answer">
-<br>
-Wrong answer 1:<br>
-<input type="text" name="wrong_answer_1">
-<br>
-Wromg answer 2:<br>
-<input type="text" name="wrong_answer_2">
-<br>
-Wrong answer 3:<br>
-<input type="text" name="wrong_answer_3">
-<br><br>
-<input type="submit" value="Submit">
-</form>
-</body>
-</html>
-""")
-
-'''
+ 
+ 
+def new_question_form(request):
+    list_of_categories = Category.find_all()
+    question_new = render_template('static/submit.html', {"list_of_categories": list_of_categories})
+    request.write(question_new)
 
 def get_question_handler(request, question_id):
   request.write("""<!DOCTYPE html>
