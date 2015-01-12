626b60c8fa6147cf13ee12acd8b23f310581eff6
lkw657 <lkw657@noreply.github.com>
2015-01-12 11:41:57 +1100

    Fixed whitespace
    Spaces not tabs!

--- tests/pages.py
+++ tests/pages.py
@@ -1,17 +1,17 @@
 from tornado.testing import AsyncHTTPTestCase
 
 class HTTPTestCase(AsyncHTTPTestCase):
-	def get_app(self):
-		from trivia import server
-		self.app = server.app()
-		return self.app
-	
-	def test_00_view_homepage(self):
-		url = '/'
-		headers = {'method': 'GET'}
-		self.check_page(url, **headers)
+    def get_app(self):
+        from trivia import server
+        self.app = server.app()
+        return self.app
+    
+    def test_00_view_homepage(self):
+        url = '/'
+        headers = {'method': 'GET'}
+        self.check_page(url, **headers)
 
-	def check_page(self, url, **headers):
-		response = self.fetch(url, **headers)
-		if response.error:
-			raise Exception('{}: {} {} {}'.format(response.error, response.code, response.request.method, response.request.url))
+    def check_page(self, url, **headers):
+        response = self.fetch(url, **headers)
+        if response.error:
+            raise Exception('{}: {} {} {}'.format(response.error, response.code, response.request.method, response.request.url))
