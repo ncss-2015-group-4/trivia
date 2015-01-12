0833c45d6948d8dc089e01fa5930dce400ac8835
Alexander <alexcollins010@gmail.com>
2015-01-13 05:53:50 +1100

    fix index again

--- game_handler.py
+++ game_handler.py
@@ -55,5 +55,5 @@ def submit_question_handler(request, answer_id):
         if game.question_index >= len(game.question_ids):
             request.redirect('/post_game/{0}'.format(score))
         else:
-            request.redirect('/game/{0}'.format(game.question_index))
+            request.redirect('/game/{0}'.format(str(int(game.question_index)+1))
         
