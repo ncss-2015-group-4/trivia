60b84dd62070c7e4665112c7da25a2c8f7426f64
Lachlan Wood <lach_wood@bigpond.com>
2015-01-12 14:56:22 +1100

    :+1:  Dynamic user name input
    into the html for the profile page :)

--- profile_handler.py
+++ profile_handler.py
@@ -1,7 +1,15 @@
 from templating import render_template
 from db.models import User
 
+
 def profile_handler(request):
-    profile_page = render_template('static/profile.html', {})
-    request.write(profile_page)
+    id = request.get_secure_cookie ('user_id')
+    if id is None:
+        request.redirect("/login")
+    else:
+        id = id.decode("UTF-8")
+        print("WLKHGKUSDKUGLUBGS:KDJ:KV:LSKDNV:LKSND:LVK------------>", id)
+        user_data = User.find(user_id=id)
+        profile_page = render_template('static/profile.html', {"user_name": user_data.username})
+        request.write(profile_page)
 
