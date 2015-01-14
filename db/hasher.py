af9823ab038b0918f0c31142a8fe364a804ba286
Harrison <harrison@theshoebridges.com>
2015-01-14 20:56:20 +1100

    db/hasher: pbkdf2_hmac password encryption

--- db/hasher.py
+++ db/hasher.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib, binascii
 
 def hash(password):
-	return hashlib.md5(password.encode()).hexdigest()
+	return binascii.hexlify(hashlib.pbkdf2_hmac('sha256', password.encode(), b'SALT_XYZ_YOLO', 5000))
