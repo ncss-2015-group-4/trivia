be854bb062cb29762bad69f6fc742f2ad653b87d
lkw657 <lkw657@users.noreply.github.com>
2015-01-16 10:42:08 +0800

    Finished documenting templating.py

--- templating.py
+++ templating.py
@@ -20,6 +20,7 @@ class Node(object):
     Defines a basic node object
     '''
     def __init__(self, content):
+        #stuff inside the node
         self.content = content
     def eval(self, scope):
         raise NotImplementedError()
@@ -27,7 +28,7 @@ class Node(object):
 class PythonNode(Node):
     '''
     Defines a node for the execution of python code
-    Syntax: {{<python_expression}}
+    Syntax: {{ python_expression }}
     '''
     def eval(self, scope):
         #return the execution of the python code
@@ -45,21 +46,24 @@ class GroupNode(Node):
     Defines a node to group nodes in the same area, e.g within a for/if statement
     '''
     def __init__(self, children):
+        #List of nodes that the group node contains
         self.children = children
     def eval(self, scope):
         result = ''
-        #evaluate all the child nodes and add them to output
+        #Evaluate all the child nodes and add them to output
         for node in self.children:
             result += str(node.eval(scope))
         return result
-        
+ 
 class IfNode(Node):
     '''
     Defines a node for execution if statements
-    Syntax: {% if <condition> %}<stuff>{% end if %}
+    Syntax: {% if <condition> %}stuff{% end if %}
     '''
     def __init__(self, predicate, true_node):
+        #Part to be evaluated to true or false
         self.predicate = predicate
+        #Group node to be executed if the predicate is true
         self.true_node = true_node
 
     def eval(self, scope):
@@ -74,37 +78,48 @@ class IncludeNode(Node):
     Syntax: {% include <file> %}
     '''
     def __init__(self, path):
+        #Path to the file to be included
         self.path = path.strip()
-        
+
     def eval(self, scope):
+        #Open the file
         with open(self.path) as p:
-            lines=[line.strip() for line in p]
-            template=''.join(lines)
+            #Read the file
+            lines = [line.strip() for line in p]
+            template = ''.join(lines)
+        #return the evaluation of the file
         return Parser(template).eval(scope)
-            
-class ForNode():
+
+class ForNode(Node):
     '''
     Defines a node for executing for loops
     Syntax: {% for <var_name> in <itterable_name> %}<stuff>{% end for %}
     '''
     def __init__(self, var_name, itterable_name, true_node):
+        #Name of the variable to store content from the itterable
         self.var_name = var_name
+        #Varible to be itterated over
         self.itterable_name = itterable_name
+        #Group node to be executed in every itteration
         self.true_node = true_node
-    
+
     def eval(self, scope):
         output = ''
         modified_scope = scope
-        itterable = eval(self.itterable_name ,scope)
+        #Get the itterable
+        itterable = eval(self.itterable_name, scope)
+        #Itterate over the itterable
         for var in itterable:
+            #Set var_name to be the contents of the current position in itterable
             modified_scope[self.var_name] = var
+            #Evaluate the group node and concatenate it to the output
             output += str(self.true_node.eval(modified_scope)).strip()
         return output
 
 class Parser(object):
     '''
     Class used to parse a template file into html
-    
+
     >>> Parser("abcd{{1+1}}efgh{% if 1==1 %}2{% end if %}{% if 1==3 %}3{% end if %}1234").eval({})
     'abcd2efgh21234'
     >>> Parser("{% if 1==1 %}2{% end if %}{{5*2}}{% if 1==3 %}3{% end if %}1234").eval({})
@@ -120,10 +135,13 @@ class Parser(object):
     >>> Parser("abc{% for i in items %}something{{i}}{% end for %}def").eval({'items':[1,2,3]})
     'abcsomething1something2something3def'
     '''
-    
+
     def __init__(self, tokens):
+        #String to be parsed
         self.tokens = tokens
+        #Current position in tokens
         self.index = 0
+        #Length of tokens
         self.length = len(tokens)
 
     def _parse_python_node(self):
@@ -131,6 +149,7 @@ class Parser(object):
         Function to parse a python node
         '''
         content = ''
+        #Add the current position to the content
         content += self.tokens[self.index]
         #all everything before the end of the node into content
         while self.read_next() != '}':
@@ -150,13 +169,15 @@ class Parser(object):
         Function to parse a text node
         '''
         content = ''
+        #Add the current token to content
         content += self.tokens[self.index]
+        #Keep adding to content until a '{' is reached
         while not self.is_end() and self.read_next() != '{':
             self.next()
             content += self.tokens[self.index]
         self.next()
         return TextNode(content)
-        
+
     def _parse_if_node(self):
         '''
         Function to parse an if node
@@ -173,12 +194,12 @@ class Parser(object):
         #check for the end of the if decleration
         if self.read_next() != '}':
             raise ParseError("'}' expected")
-        self.next() 
+        self.next()
         self.next()
         #create a GroupNode for all the stuff in the for loop
         true_node = self._parse_group_node('{% end if %}')
         return IfNode(predicate, true_node)
-        
+
     def _parse_for_node(self):
         '''
         Function to parse a for node
@@ -193,13 +214,13 @@ class Parser(object):
             if self.is_end():
                 raise ParseError("Unexpected end of input.")
         #check if the variable name exists
-        if var_name=='':
+        if var_name == '':
             raise ParseError('Missing variable name.')
         self.next()
         #check for " in "
-        if self.tokens[self.index:self.index+4]!= ' in ':
+        if self.tokens[self.index:self.index+4] != ' in ':
             raise ParseError("'in' expected.")
-        self.index+=3
+        self.index += 3
         #find the name of the itterable that is being looped through
         while self.read_next() != ' ':
             self.next()
@@ -216,19 +237,21 @@ class Parser(object):
         self.next()
         if self.read_next() != '}':
             raise ParseError("'}' expected")
-        self.next() 
+        self.next()
         self.next()
         #create a GroupNode for all the stuff in the foor loop
         true_node = self._parse_group_node('{% end for %}')
         return ForNode(var_name, itterable_name, true_node)
-        
+
     def _parse_include_node(self):
         '''
         Function to parse an include node
         '''
         path = ''
+        #Add current token to that path of file to be included
         path += self.tokens[self.index]
         while self.read_next() != '%':
+            #Add to the path until an '%' is reached
             self.next()
             path += self.tokens[self.index]
             if self.is_end():
@@ -236,44 +259,50 @@ class Parser(object):
         self.next()
         if self.read_next() != '}':
             raise ParseError("'}' expected")
-        self.next() 
+        self.next()
         self.next()
         return IncludeNode(path)
-    
+
     def _parse_group_node(self, end):
         '''
         Function to group nodes in similar areas together into group nodes
         '''
         children = []
+        #While not at the end of the node, or the end of the file
         while self.tokens[self.index:self.index+len(end)] != end or (end == '' and not self.is_end()):
             if self.is_end():
                 return ParseError("Unexpected end of input.")
+            #check for a '{'
             if self.tokens[self.index] == '{':
                 self.next()
+                #if there is another '{' it must be a python statement
                 if self.tokens[self.index] == '{':
                     self.next()
                     children.append(self._parse_python_node())
-                
+                #If there is a '%' it could be include, if, or for
                 elif self.tokens[self.index] == '%':
                     self.next()
+                    #Check if it is an if
                     if self.tokens[self.index:self.index+len(IF_TAG)] == IF_TAG:
                         self.index += len(IF_TAG)
                         children.append(self._parse_if_node())
-                
+                    #check if it is an include
                     elif self.tokens[self.index:self.index+len(INCLUDE_TAG)] == INCLUDE_TAG:
                         self.index += len(INCLUDE_TAG)
                         children.append(self._parse_include_node())
-                    
+                    #check if it is a for
                     elif self.tokens[self.index:self.index+len(FOR_TAG)] == FOR_TAG:
                         self.index += len(FOR_TAG)
                         children.append(self._parse_for_node())
                 else:
+                    #Otherwise it is an error
                     raise ParseError("Unexpected token after '{'")
             else:
+                #If there was not a '{' then it is a text node
                 children.append(self._parse_text_node())
         self.index += len(end)
         return GroupNode(children)
-        
+
     def parse(self):
         '''
         Function to start parsing
@@ -282,25 +311,37 @@ class Parser(object):
         if not self.is_end():
             raise ParseError("Extra input")
         return node
-    
+
     def eval(self, scope):
         return self.parse().eval(scope)
 
     def read_next(self):
+        '''
+        Return the next character in tokens
+        '''
         return self.tokens[self.index + 1]
 
     def is_end(self):
+        '''
+        Check if we are at the ned of tokens
+        '''
         return self.index >= self.length - 1
 
     def next(self):
+        '''
+        Incriment the current index
+        '''
         self.index += 1
 
 def render_template(path, scope):
+    '''
+    Function which uses the Parser class to parse a html file
+    '''
     with open(path) as p:
-        lines=[line.strip() for line in p]
-        template=''.join(lines)
+        lines = [line.strip() for line in p]
+        template = ''.join(lines)
     return Parser(template).eval(scope)
-  
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
