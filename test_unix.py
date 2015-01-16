62146af93af10e51b76ea3a16f2d96cf994dd780
lkw657 <lkw657@users.noreply.github.com>
2015-01-16 14:47:12 +0800

    Added database creation and dummy data scripts to python test files I have no idea if test.py will work as I do not use windows

--- test_unix.py
+++ test_unix.py
@@ -2,16 +2,28 @@ import sys
 from tornado.testing import main
 import subprocess
 
+#set up database
+process = subprocess.Popen(["cd", "db", "&&", "python3", "create_db.py"], stderr=subprocess.PIPE)
+if process.stderr.read() == b'':
+    print("Created database")
+else:
+    print(process.stderr.read())
+process = subprocess.Popen(["cd", "db", "&&", "python3", "dummy_data.py"], stderr=subprocess.PIPE)
+if process.stderr.read() == b'':
+    print("Populated database with dummy data")
+else:
+    print(process.stderr.read())
+
 def Test_docstrings(file):
-    print("Testing docstrings in "+file)
-    process=subprocess.Popen(["python3",file], stderr=subprocess.PIPE)
-    if process.stderr.read()==b'':
+    print("Testing docstrings in " + file)
+    process = subprocess.Popen(["python3", file], stderr=subprocess.PIPE)
+    if process.stderr.read() == b'':
         print("OK")
     else:
         print(process.stderr.read())
 
 Test_docstrings("templating.py")
-sys.argv=["test_unix.py","tests/pages.py"]
+sys.argv=["test_unix.py", "tests/pages.py"]
 main()
 input("----Press enter to continue----")
 
