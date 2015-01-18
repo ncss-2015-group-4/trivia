e28120ff6ebb9233d6676fae81eeacfd5deb60ef
lkw657 <lkw657@users.noreply.github.com>
2015-01-18 12:29:18 +0800

    Added tests for the category pages Also fixed a problem where I defined exceptions as functions instead of classes

--- tests/pages.py
+++ tests/pages.py
@@ -2,6 +2,9 @@ from tornado.testing import AsyncHTTPTestCase
 from tornado.web import create_signed_value
 import re
 from db.models import User
+from db.models import Category
+from db.models import Question
+import html
 #define regex patters to search for nav bar links
 pre_game_pattern = re.compile(r'href\ *\=\ *\"\/pre_game\"')
 submit_pattern = re.compile(r'href\ *\=\ *\"\/question\"')
@@ -13,21 +16,33 @@ link_patterns = {'pre_game': pre_game_pattern, 'submit': submit_pattern, 'home':
 
 class MissingLink(Exception):
     '''
-    Exception for if a link is missing in html
+    Exception for if a link is missing in page_html
     '''
     pass
 
-def LoginError(Exception):
+class LoginError(Exception):
     '''
     Exception for an issue with login/register
     '''
     pass
+
+class GameError(Exception):
+    '''
+    Exception for an issue with the game
+    '''
+    pass
+
+class PageError(Exception):
+    '''
+    Exception for an error with a page
+    '''
+    pass
     
-def check_link(html, link, page):
+def check_link(page_html, link, page):
     '''
-    Checks if a link is in some html
+    Checks if a link is in some page_html
     '''
-    if not link_patterns[link].search(html):
+    if not link_patterns[link].search(page_html):
         raise MissingLink("The "+link+" link is missing from the "+page+" page.")
 
 
@@ -40,46 +55,46 @@ class HTTPTestCase(AsyncHTTPTestCase):
     def test_00_homepage_tests(self):
         url = '/'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)
-        check_link(html, "home", "home")
-        check_link(html, "pre_game", "home")
-        check_link(html, "submit", "home")
-        #check_link(html, "logout", "home")
-        #check_link(html, "profile", "home")
+        page_html = self.check_page(url, **headers)
+        check_link(page_html, "home", "home")
+        check_link(page_html, "pre_game", "home")
+        check_link(page_html, "submit", "home")
+        #check_link(page_html, "logout", "home")
+        #check_link(page_html, "profile", "home")
         
     def test_01_login(self):
         url = '/login'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)#test if the login page can be accessed
-        check_link(html, "home", "login")
-        check_link(html, "pre_game", "login")
-        check_link(html, "submit", "login")
-        #check_link(html, "logout", "login")
-        #check_link(html, "profile", "login")
+        page_html = self.check_page(url, **headers)#test if the login page can be accessed
+        check_link(page_html, "home", "login")
+        check_link(page_html, "pre_game", "login")
+        check_link(page_html, "submit", "login")
+        #check_link(page_html, "logout", "login")
+        #check_link(page_html, "profile", "login")
         
     def test_02_register(self):
         url = '/user'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)#test if the user page can be accessed
+        page_html = self.check_page(url, **headers)#test if the user page can be accessed
         '''
-        check_link(html,"home","user")
-        check_link(html,"pre_game","user")
-        check_link(html,"submit","user")
-        check_link(html, "submit", "user")
-        #check_link(html,"profile","user")
+        check_link(page_html,"home","user")
+        check_link(page_html,"pre_game","user")
+        check_link(page_html,"submit","user")
+        check_link(page_html, "submit", "user")
+        #check_link(page_html,"profile","user")
         '''
         #test registration
         url= '/user'
         headers = {'method': 'POST', 'body': b'username=testUser&password=testPass&email=testUser%40someDomain.com'}
-        html=self.check_page(url, **headers)
+        page_html=self.check_page(url, **headers)
         user_id=User.find(username='testUser').id
         cookie_value = create_signed_value(self.app.settings['cookie_secret'], 'user_id', str(user_id))
         cookies = 'user_id='+cookie_value.decode('utf-8')
         #check if user name appears on homepage
         url='/'
         headers = {'method': 'GET', 'headers': {'Cookie': cookies}}
-        html = self.check_page(url, **headers)
-        if 'testUser' not in html:
+        page_html = self.check_page(url, **headers)
+        if 'testUser' not in page_html:
             raise LoginError('Username not on the homepage')
         else:
             print('Logged in after registering')
@@ -87,66 +102,80 @@ class HTTPTestCase(AsyncHTTPTestCase):
     def test_03_profile_tests(self):
         url = '/profile'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)
-        check_link(html, "home", "profile")
-        check_link(html,"pre_game", "profile")
-        check_link(html, "submit", "profile")
-        #check_link(html, "logout", "profile")
-        #check_link(html, "profile", "profile")
+        page_html = self.check_page(url, **headers)
+        check_link(page_html, "home", "profile")
+        check_link(page_html,"pre_game", "profile")
+        check_link(page_html, "submit", "profile")
+        #check_link(page_html, "logout", "profile")
+        #check_link(page_html, "profile", "profile")
         
     def test_04_question_submission_tests(self):
         url = '/submit'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)
-        check_link(html, "home", "sumbission")
-        check_link(html, "pre_game", "submission")
-        check_link(html, "submit", "submission")
-        #check_link(html, "logout", "submission")
-        #check_link(html, "profile", "submission")
+        page_html = self.check_page(url, **headers)
+        check_link(page_html, "home", "sumbission")
+        check_link(page_html, "pre_game", "submission")
+        check_link(page_html, "submit", "submission")
+        #check_link(page_html, "logout", "submission")
+        #check_link(page_html, "profile", "submission")
         
-    def test_05_pre_game_tests(self):
-        url = '/pre_game'
+    def test_05_category_tests(self):
+        url = '/categories'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)
-        check_link(html, "home", "pre_game")
-        check_link(html, "pre_game", "pre_game")
-        check_link(html, "submit", "pre_game")
-        #check_link(html, "logout", "pre_game")
-        #check_link(html, "profile", "pre_game")
-        
+        page_html = self.check_page(url, **headers)
+        check_link(page_html, "home", "categories")
+        check_link(page_html, "pre_game", "categories")
+        check_link(page_html, "submit", "categories")
+        #check if all the categories are listed
+        list_of_categories = Category.find_all()
+        #Checking that all the categories are listed
+        for category in list_of_categories:
+            print('Checking if category '+str(category.id)+' is listed on the categories page')
+            if not re.search(r'\<a href\=\"\/category\/'+str(category.id)+r'\"\>\<button class\=\"category\-button\"\>'+re.escape(html.escape(category.name))+r'\<\/button\>\<\/a>\<\/br\>', page_html):
+                raise PageError(category.name+' is missing from the categoties page.')
+        #Check that all the questions are listed
+        for category in list_of_categories:
+            print('Checking question list in category '+str(category.id))
+            list_of_questions = Question.find_all(category=category.id)
+            url = '/category/'+str(category.id)
+            page_html = self.check_page(url, **{'method': 'GET'})
+            for question in list_of_questions:
+                if not re.search(r'\<li\>'+re.escape(html.escape(question.question))+r'\<\/li\>', page_html):
+                    raise PageError(str(question.question)+' is missing from '+url)
+ 
     def test_06_game_tests(self):
         pass
         '''
         url = '/game'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers).decode()
-        check_link(html, "home", "game")
-        check_link(html, "pre_game", "game")
-        check_link(html, "submit", "game")
-        check_link(html, "logout", "game")
-        #check_link(html, "profile", "game")
+        page_html = self.check_page(url, **headers).decode()
+        check_link(page_html, "home", "game")
+        check_link(page_html, "pre_game", "game")
+        check_link(page_html, "submit", "game")
+        check_link(page_html, "logout", "game")
+        #check_link(page_html, "profile", "game")
         '''
 
     '''
     def test_07_post_game_tests(self):
         url = '/post_game'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers).decode()
-        check_link(html, "home", "login")
-        check_link(html, "pre_game", "login")
-        check_link(html, "submit", "login")
-        #check_link(html, "logout", "login")
-        #check_link(html, "profile", "login")
+        page_html = self.check_page(url, **headers).decode()
+        check_link(page_html, "home", "login")
+        check_link(page_html, "pre_game", "login")
+        check_link(page_html, "submit", "login")
+        #check_link(page_html, "logout", "login")
+        #check_link(page_html, "profile", "login")
     '''
 
     def test_07_logout_tests(self):
         url = '/logout'
         headers = {'method': 'GET'}
-        html = self.check_page(url, **headers)
-        check_link(html, "home", "logout")
-        check_link(html, "pre_game", "logout")
-        #check_link(html, "submit", "logout")
-        #check_link(html, "profile", "logout")
+        page_html = self.check_page(url, **headers)
+        check_link(page_html, "home", "logout")
+        check_link(page_html, "pre_game", "logout")
+        #check_link(page_html, "submit", "logout")
+        #check_link(page_html, "profile", "logout")
     
     def check_page(self, url, **headers):
         response = self.fetch(url, **headers)
