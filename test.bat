77851d756ee7cf80035a1f8dafaf7cf88917c0e1
David <auscompgeek@users.noreply.github.com>
2015-03-08 23:27:29 +1100

    test: make test scripts proper scripts
    * Print messages that make sense.
    
    test.sh:
    * Always clean up on exit.
    * Return the exit code of tests that fail.

--- test.bat
+++ test.bat
@@ -1,15 +1,24 @@
 @echo off
+setlocal
+cd /d "%~dp0"
+
 cd db
-echo Backing up database (If one exists)
-if exist trivia.db ren trivia.db triviaBackup.db
-echo Creating new database (in case one does not already exist)
+if exist trivia.db (
+	echo Existing database found, backing up.
+	ren trivia.db triviaBackup.db
+)
+
+echo Initialising database...
 py -3 create_db.py
 py -3 dummy_data.py
 cd ..
-echo Running tests
+
+echo Running tests...
 py -3 templating.py
+py -3 -m db.models
 py -3 -m tornado.testing tests/pages.py
-echo Restoring database from backup
+
+echo Cleaning up...
 cd db
 del trivia.db
 if exist triviaBackup.db ren triviaBackup.db trivia.db
--- test.sh
+++ test.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
+status=0
+
+cleanup_exit() {
+	echo 'Cleaning up...'
+	cd db
+	rm trivia.db
+	if [ -f triviaBackup.db ]; then
+		mv triviaBackup.db trivia.db
+	fi
+
+	exit $status
+}
+
+cd "$(dirname "$0")"
+
 cd db
-echo Backing up database \(If one exists\)
-if [ -a trivia.db  ]; then mv trivia.db triviaBackup.db; fi
-echo Creating new database \(in case one does not already exist\)
+if [ -e trivia.db ]; then
+	echo 'Existing database found, backing up.'
+	mv trivia.db triviaBackup.db
+fi
+
+# make sure we clean up the test database afterwards
+trap cleanup_exit EXIT HUP INT TERM
+
+echo 'Initialising database...'
 python3 create_db.py
 python3 dummy_data.py
 cd ..
-echo running tests
-python3 templating.py
-python3 -m tornado.testing tests/pages.py
-echo Restoring database from backup
-cd db
-rm trivia.db
-if [ -a triviaBackup.db  ]; then mv triviaBackup.db trivia.db; fi
+
+echo 'Running tests...'
+python3 templating.py || status=$?
+python3 -m db.models || status=$?
+python3 -m tornado.testing tests/pages.py || status=$?
