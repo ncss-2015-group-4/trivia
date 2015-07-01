a076ab6c51b7475adb5b6f13da28e2daf6eca0c5
David <auscompgeek@users.noreply.github.com>
2015-07-01 18:44:30 +1000

    trivia: actually use argparse, refactor

--- trivia.py
+++ trivia.py
@@ -18,34 +18,39 @@ from handlers.category import category_handler, category_list_handler
 from handlers.question import new_question_handler, new_question_form, edit_question_handler
 from handlers.logout import logout_handler
 
+
+def new_server(port=8888, hostname='', debug=True):
+    server = Server(port=port, hostname=hostname, debug=debug)
+
+    server.register('/', index_handler)
+    server.register('/profile', profile_handler)
+    server.register('/game/([0-9]+)', get_question_handler)
+    server.register('/game/submit/([0-9]+)', submit_question_handler)
+    server.register('/game/create', game_handler)
+    server.register('/pre_game', pre_game_handler)
+    server.register('/post_game', post_game_handler)
+    server.register('/submit', submit_handler)
+    server.register('/leaderboard', leaderboard_handler)
+    server.register('/login', login_handler, post=login_handler_post)
+    server.register('/question', new_question_form, post=new_question_handler)
+    server.register('/question/([0-9]+)', get_question_handler, post=edit_question_handler)
+    server.register('/category/([0-9]+)', category_handler)
+    server.register('/user', user_handler, post=signup_handler_post)
+    # server.register('/user/([0-9]+)', edit_user_handler)
+    server.register('/categories', category_list_handler)
+    server.register('/logout', logout_handler)
+    server.register('/.*', error_handler)
+
+    return server
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", help="what port do you want the server to listen on?", type=int)
-    parser.add_argument("--hostname", help="what host do you want the server to run on?", type=str)
+    parser = argparse.ArgumentParser(description='The Quizzi server, a social trivia website.')
+    parser.add_argument('-p', '--port', type=int, default=8888, help='port to listen on')
+    parser.add_argument('-H', '--hostname', default='', help='hostname to bind to')
     args = parser.parse_args()
 
-    server = Server(port=args.port or 8888, hostname=args.hostname or '')
-else:
-    server = Server(debug=False)
-
-server.register('/', index_handler)
-server.register('/profile', profile_handler)
-server.register('/game/([0-9]+)', get_question_handler)
-server.register('/game/submit/([0-9]+)', submit_question_handler)
-server.register('/game/create', game_handler)
-server.register('/pre_game', pre_game_handler)
-server.register('/post_game', post_game_handler)
-server.register('/submit', submit_handler)
-server.register('/leaderboard', leaderboard_handler)
-server.register('/login', login_handler, post=login_handler_post)
-server.register('/question', new_question_form, post=new_question_handler)
-server.register('/question/([0-9]+)', get_question_handler, post=edit_question_handler)
-server.register('/category/([0-9]+)', category_handler)
-server.register('/user', user_handler, post=signup_handler_post)
-# server.register('/user/([0-9]+)', edit_user_handler)
-server.register('/categories', category_list_handler)
-server.register('/logout', logout_handler)
-server.register('/.*', error_handler)
-
-if __name__ == '__main__':
+    server = new_server(port=args.port, hostname=args.hostname)
     server.run()
+else:
+    server = new_server(debug=False)
