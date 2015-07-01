582209b1188875a68b0c51d95e8c398db66563c2
David <auscompgeek@users.noreply.github.com>
2015-07-01 21:31:53 +1000

    dummy_data: make dummy data predictable
    Since commit 28554956, questions were added in an unpredictable order,
    due to PEP 456.  This commit avoids the use of dicts altogether, instead
    adding each category in the order specified in categories.csv.
    
    This will allow for more stringent testing of the database API.

--- db/dummy_data.py
+++ db/dummy_data.py
@@ -6,41 +6,26 @@ import hasher
 
 conn = sqlite3.connect('trivia.db')
 cur = conn.cursor()
-categories = {} 
-category_files = {} 
 difficulties = {"easy":0, "medium":1, "hard":2}
 with open("categories.csv") as f:
-    category_reader = csv.DictReader(f)
-    #Get the list of categories
-    for i,category in enumerate(category_reader):
-        #store the category id
-        categories[category["Category"]] = i+1
-        #store the category csv file name
-        category_files[category["Category"]] = category["CSV File"]
+    question_id = 1
+    for i, category in enumerate(csv.DictReader(f)):
+        cat_id = i+1
+        # add the category to the database
+        cur.execute("INSERT INTO categories VALUES(?, ?)", (cat_id, category["Category"]))
 
-for category in categories:
-    #category id, category name
-    cur.execute("INSERT INTO categories VALUES(?, ?)", (categories[category], category))
-
-question_count = 1
-answer_count = 1
-for category, category_file in category_files.items():
-    with open(category_file) as f:
-        reader = csv.DictReader(f)
-        #for each question in the category
-        for question in reader:
-            #add the question
-            #question id, question text, times answered, times correct, category id, difficulty
-            cur.execute('INSERT INTO questions VALUES(?, ?, 0, 0, ?, ?)', (question_count, question['Question'], categories[category], difficulties[question['Difficulty']]))
-            #answer id, qyestion id, is the correct answer, answer text
-            #add the correct answer
-            cur.execute('INSERT INTO answers VALUES(?, ?, 1, ?)', (answer_count, question_count, question['Correct Answer']))
-            answer_count += 1
-            #add the incorrect answers
-            for i in range(1, 4):
-                cur.execute('INSERT INTO answers VALUES(?, ?, 0, ?)', (answer_count, question_count, question['Wrong Answer '+str(i)]))
-                answer_count += 1
-            question_count += 1
+        with open(category["CSV File"]) as fc:
+            for question in csv.DictReader(fc):
+                # add the question
+                # question id, question text, times answered, times correct, category id, difficulty
+                cur.execute('INSERT INTO questions VALUES(?, ?, 0, 0, ?, ?)', (question_id, question['Question'], cat_id, difficulties[question['Difficulty']]))
+                # answer id, question id, is the correct answer, answer text
+                # add the correct answer
+                cur.execute('INSERT INTO answers VALUES(NULL, ?, 1, ?)', (question_id, question['Correct Answer']))
+                # add the incorrect answers
+                for j in range(1, 4):
+                    cur.execute('INSERT INTO answers VALUES(NULL, ?, 0, ?)', (question_id, question['Wrong Answer %d' % j]))
+                question_id += 1
 
 def add_user(username, password, email):
     salt = hasher.new_salt()
