478aade1566000048f9942ba8597120c5475c9c6
David <auscompgeek@users.noreply.github.com>
2015-07-02 12:44:23 +1000

    category: clean up
    * Use the new grab_user decorator.
    * Use .find_iter()!  How exciting.
    * Don't throw an exception when the requested category doesn't exist.

--- handlers/category.py
+++ handlers/category.py
@@ -1,38 +1,36 @@
 from templating import render_template
-from db.models import Category
-from db.models import User
-from db.models import Question
-from . import template_paths
+from db.models import Category, Question
+from .error import create_error
+from . import template_paths, grab_user
 
+
+@grab_user
 def category_handler(request, category_id):
     cat = Category.find(category_id=category_id)
+    if not cat:
+        create_error(request, "That category does not exist.")
+        return
 
-    list_of_questions = Question.find_all(category=category_id)
-    #print(list_of_questions)
-    u_id = request.get_secure_cookie('user_id')
     u_name = ""
-    if u_id is not None:
-        u_id = u_id.decode("UTF-8")
-        u_name = User.find(user_id=u_id)
-        u_name = u_name.username
+    if request.user:
+        u_name = request.user.username
 
     category_page = render_template(template_paths["submit_category"], {
         "user_name": u_name,
         "category_name": cat.name,
-        'list_of_questions': list_of_questions})
+        'questions': Question.find_iter(category=category_id)
+    })
     request.write(category_page)
 
-def category_list_handler(request):
-    u_id = request.get_secure_cookie('user_id')
-    u_name = ""
-    if u_id is not None:
-        u_id = u_id.decode("UTF-8")
-        u_name = User.find(user_id=u_id)
-        u_name = u_name.username
 
-    list_of_categories = Category.find_all()
-    print(list_of_categories)
+@grab_user
+def category_list_handler(request):
+    u_name = ""
+    if request.user:
+        u_name = request.user.username
+
     list_categories_page = render_template(template_paths["categories"], {
         "user_name": u_name,
-        "list_of_categories": list_of_categories})
+        "categories": Category.find_iter()
+    })
     request.write(list_categories_page)
