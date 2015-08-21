42d3ac81ae8a23400bb0498ac6fd6fbb09918ab5
David <auscompgeek@users.noreply.github.com>
2015-08-21 20:12:10 +1000

    trivia: Slight clean up

--- trivia.py
+++ trivia.py
@@ -9,7 +9,6 @@ from handlers.profile import profile_handler
 from handlers.game import game_handler, get_question_handler, submit_question_handler
 from handlers.pre_game import pre_game_handler
 from handlers.post_game import post_game_handler
-from handlers.submit import submit_handler
 from handlers.login import login_handler, login_handler_post, signup_handler_post
 from handlers.user import user_handler
 from handlers.error import error_handler
@@ -24,22 +23,20 @@ def new_server(port=8888, hostname='', debug=True):
 
     server.register('/', index_handler)
     server.register('/profile', profile_handler)
-    server.register('/game/([0-9]+)', get_question_handler)
-    server.register('/game/submit/([0-9]+)', submit_question_handler)
+    server.register(r'/game/([0-9]+)', get_question_handler)
+    server.register(r'/game/submit/([0-9]+)', submit_question_handler)
     server.register('/game/create', game_handler)
     server.register('/pre_game', pre_game_handler)
     server.register('/post_game', post_game_handler)
-    server.register('/submit', submit_handler)
     server.register('/leaderboard', leaderboard_handler)
     server.register('/login', login_handler, post=login_handler_post)
-    server.register('/question', new_question_form, post=new_question_handler)
-    server.register('/question/([0-9]+)', get_question_handler, post=edit_question_handler)
-    server.register('/category/([0-9]+)', category_handler)
+    server.register(r'/(?:question|submit)', new_question_form, post=new_question_handler)
+    server.register(r'/question/([0-9]+)', get_question_handler, post=edit_question_handler)
+    server.register(r'/category/([0-9]+)', category_handler)
     server.register('/user', user_handler, post=signup_handler_post)
-    # server.register('/user/([0-9]+)', edit_user_handler)
     server.register('/categories', category_list_handler)
     server.register('/logout', logout_handler)
-    server.register('/.*', error_handler)
+    server.register(r'/.*', error_handler)
 
     return server
 
