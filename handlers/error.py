8e4c22c314c34e91164a86cc926bb96cecfbfbbb
David <auscompgeek@users.noreply.github.com>
2015-09-05 13:53:37 +1000

    error: clean up and allow arbitrary status
    * Use the @grab_user decorator.
    * Allow for sending non-404 errors.

--- handlers/error.py
+++ handlers/error.py
@@ -1,19 +1,18 @@
-from . import template_paths
+from . import template_paths, grab_user
 from templating import render_template
-from db.models import User
+
 
 def error_handler(request):
     create_error(request, "404 Error!")
-    
-def create_error(request, message):
-    request.set_status(404)
 
-    u_id = request.get_secure_cookie('user_id')
+
+@grab_user
+def create_error(request, message, status=404):
+    request.set_status(status)
+
     u_name = ""
-    if u_id is not None:
-        u_id = u_id.decode("UTF-8")
-        u_name = User.find(user_id=u_id)
-        u_name = u_name.username
+    if request.user:
+        u_name = request.user.username
 
     error_page = render_template(template_paths["error"], {"user_name": u_name, "error": message})
     request.write(error_page)
