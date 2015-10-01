2dbf45fcdb7a42e31b4e0f33ceb5f451340106c0
David <auscompgeek@users.noreply.github.com>
2015-10-01 13:21:50 +1000

    db: Separate schema from create_db.py
    Separate the schema out into a separate file to make reading/editing
    the database schema easier.

---
+++ db/schema.sql
@@ -0,0 +1,72 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    salt TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    question_id INTEGER PRIMARY KEY,
+    question TEXT NOT NULL,
+    questions_answered INTEGER NOT NULL,
+    questions_correct INTEGER NOT NULL,
+    category TEXT NOT NULL,
+    difficulty REAL NOT NULL
+);
+
+CREATE TABLE categories (
+    category_id INTEGER PRIMARY KEY,
+    category TEXT NOT NULL
+);
+
+CREATE TABLE answers (
+    answer_id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    correct BOOLEAN NOT NULL,
+    answer_text TEXT NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions (question_id)
+);
+
+CREATE TABLE flags (
+    flag_id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions (question_id)
+);
+
+CREATE TABLE scores (
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    num_answered INTEGER NOT NULL,
+    num_correct INTEGER NOT NULL,
+    PRIMARY KEY (user_id, category_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+CREATE TABLE games (
+    game_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    questions TEXT NOT NULL,
+    question_index INTEGER NOT NULL,
+    time_started INTEGER NOT NULL,
+    time_completed INTEGER,
+    difficulty REAL NOT NULL,
+    category_id INTEGER NOT NULL,
+    score INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+CREATE TABLE questionresults (
+    game_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    answer_id INTEGER,
+    correct BOOLEAN,
+    PRIMARY KEY (game_id, question_id, user_id),
+    FOREIGN KEY (game_id) REFERENCES games (game_id),
+    FOREIGN KEY (question_id) REFERENCES questions (question_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (answer_id) REFERENCES answers (answer_id)
+);
